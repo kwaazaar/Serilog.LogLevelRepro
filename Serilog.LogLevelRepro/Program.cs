d749f7b231ab21cabf29e5b88235fb49e39db931
Robert te Kaat <robert.te.kaat@gmail.com>
2016-11-14 09:43:43 +0100

    Repro for Serilog issue

--- Serilog.LogLevelRepro/Program.cs
+++ Serilog.LogLevelRepro/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,29 @@ namespace Serilog.LogLevelRepro
     {
         static void Main(string[] args)
         {
+            // Set up logging
+            ILoggerFactory loggerFactory = new LoggerFactory();
+
+            // MS console
+            loggerFactory.AddConsole(LogLevel.Trace); // Trace=Most detailed on Microsoft.Extensions.Logging
+
+            // Serilog console
+            var configuration = new LoggerConfiguration()
+                .Enrich.WithProperty("servername", Environment.MachineName)
+                .WriteTo.ColoredConsole(restrictedToMinimumLevel: Events.LogEventLevel.Verbose, // Verbose=Most detailed on Serilog
+                    outputTemplate: "SERILOG: [{Level}]: {Message}{NewLine}");  // Specific template, to make it stand out on the console
+            loggerFactory.AddSerilog(configuration.CreateLogger());
+
+            // Log on all levels
+            var logger = loggerFactory.CreateLogger<Program>();
+            logger.LogCritical("**critical**");
+            logger.LogDebug("**debug**"); // Not logged by Serilog!
+            logger.LogError("**error**");
+            logger.LogInformation("**info**");
+            logger.LogTrace("**trace**"); // Not logged by Serilog!
+            logger.LogWarning("**warning**");
+
+            Console.ReadKey();
         }
     }
 }
